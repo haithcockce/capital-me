9a97dfb0fd7ee5ab9608992252585aa69df75874
Charles Haithcock <haithcockce@gmail.com>
2020-03-31 18:13:09 -0600

    beginning containerizing django

--- install.sh
+++ install.sh
@@ -4,7 +4,15 @@ source config
 
 # Setup postgres
 
-podman run -it --name CONTAINER_POSTGRES_NAME \
-       	-v CONTAINER_POSTGRES_MOUNT:PGDATA \
-       	-e POSTGRES_USER='capital-me' -e POSTGRES_PASSWORD='capital-me' -d --rm postgres
+podman run -it \
+	--name "$CM_CONTAINER_POSTGRES_NAME" \
+       	-v "$CM_CONTAINER_POSTGRES_VOLUME":"$CM_PGDATA" \
+       	-e POSTGRES_USER="$CM_POSTGRES_USER" \
+       	-e POSTGRES_PASSWORD="$CM_POSTGRES_PASSWORD" \
+	-d --rm \
+	postgres
 
+podman build ./django/ -t "$CM_CONTAINER_DJANGO_TAG"
+podman run -it \
+	--name "$CM_CONTAINER_DJANGO_NAME" \
+	-v "$CM_CONTAINER_DJANGO_VOLUME":"$CM_DJANGO_ROOT_DIR"
