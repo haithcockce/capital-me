2f91ccd6600b5ff1ee5735180c34326bd76448ec
Charles Haithcock <haithcockce@gmail.com>
2020-04-01 21:46:48 -0600

    Added in gunicorn and uvicorn to django. Nginx is next!

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # capital-me
 Playing around with frameworks while making a personal finances tracker
+
+### References
+
+[Dockerizing Django with Postgres, Gunicorn, and Nginx](https://testdriven.io/blog/dockerizing-django-with-postgres-gunicorn-and-nginx/)
--- install.sh
+++ install.sh
@@ -4,15 +4,15 @@ source config
 
 # Setup postgres
 
+podman build -t "$CM_CONTAINER_POSTGRES_TAG" postgres/
 podman run -it \
 	--name "$CM_CONTAINER_POSTGRES_NAME" \
        	-v "$CM_CONTAINER_POSTGRES_VOLUME":"$CM_PGDATA" \
-       	-e POSTGRES_USER="$CM_POSTGRES_USER" \
-       	-e POSTGRES_PASSWORD="$CM_POSTGRES_PASSWORD" \
 	-d --rm \
-	postgres
+	"$CM_CONTAINER_POSTGRES_TAG"
 
-podman build ./django/ -t "$CM_CONTAINER_DJANGO_TAG"
+podman build -t "$CM_CONTAINER_DJANGO_TAG" django-gunicorn/
 podman run -it \
 	--name "$CM_CONTAINER_DJANGO_NAME" \
-	-v "$CM_CONTAINER_DJANGO_VOLUME":"$CM_DJANGO_ROOT_DIR"
+	-v "$CM_CONTAINER_DJANGO_VOLUME":"$CM_DJANGO_ROOT_DIR" \
+	-d "$CM_CONTAINER_DJANGO_TAG" 
